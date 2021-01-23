2050e77b204820e40d454881421052364d58c283
jakeh1 <jacobmhenrichs@gmail.com>
2021-01-23 13:31:07 -0600

    updated the script

--- TeleOp/script.js
+++ TeleOp/script.js
@@ -1,4 +1,37 @@
 
+class SocketController{
+  constructor(){
+    this.driveTrianSocket = null;
+    this.cameraSocket = null;
+    this.setUp = false;
+  }
+  activateSockets(url){
+
+    this.driveTrianSocket = new WebSocket(url);
+
+    driveTrianSocket.onopen = function(e) {
+      console.log("Connection established");
+      driveTrianSocket.send("[]");
+    };
+    
+    driveTrianSocket.onmessage = function(event) {
+      console.log(`received message from server: ${event.data}`);
+    };
+    
+    driveTrianSocket.onclose = function(event) {
+      if (event.wasClean) {
+        console.log(`Connection closed, code=${event.code} reason=${event.reason}`);
+      } else {
+        alert('Connection brocken');
+      }
+    };
+    
+    driveTrianSocket.onerror = function(error) {
+      alert(`<error> ${error.message}`);
+    };
+  }
+}
+
 
 var wIsDown = false;
 var aIsDown = false;
@@ -6,35 +39,16 @@ var sIsDown = false;
 var dIsDown = false;
 var shiftIsDown = false;
 var lastEvent = ""
+var activate = false;
 //var cameraSocket = new WebSocket("ws://localhost:8000");
-var driveTrianSocket = new WebSocket("ws://localhost:8001")
+//var driveTrianSocket = new WebSocket("ws://localhost:8001"); //this will not conect to anything on puropse will 
+var socketController = new SocketController();
 
 
 
 
 
-driveTrianSocket.onopen = function(e) {
-  console.log("Connection established");
-  driveTrianSocket.send("[]");
-};
-
-driveTrianSocket.onmessage = function(event) {
-  console.log(`received message from server: ${event.data}`);
-};
-
-driveTrianSocket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`Connection closed, code=${event.code} reason=${event.reason}`);
-  } else {
-    alert('Connection brocken');
-  }
-};
-
-driveTrianSocket.onerror = function(error) {
-  alert(`<error> ${error.message}`);
-};
-
-
+//keys are pressed down
 document.addEventListener("keydown", function(event){
   if(event.keyCode == 87){
     wIsDown = true;
@@ -56,9 +70,10 @@ document.addEventListener("keydown", function(event){
     shiftIsDown = true;
     output();
   }
+  
 })
 
-
+//keys are up
 document.addEventListener("keyup", function(event){
   if(event.keyCode == 87){
     wIsDown = false;
@@ -104,7 +119,9 @@ function output(){
   if(!(lastEvent == val)){
     console.log(val);
     lastEvent = val;
-    driveTrianSocket.send(val);
+    if(socketController.setUp){
+      socketController.driveTrianSocket.send(val);
+    }
   }
 }
 
